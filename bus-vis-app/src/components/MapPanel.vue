5ac3c27fd8caf4882d27fbd2ce06aaa2316b919b
sarkunz <sarahmykelle@gmail.com>
2021-12-17 12:37:51 -0700

    New calculations for bus location. WORKS but the routes aren't sorted

--- bus-vis-app/src/components/MapPanel.vue
+++ bus-vis-app/src/components/MapPanel.vue
@@ -131,40 +131,56 @@ export default {
             // sum of total distance
             return Math.sqrt(dist0 * dist0 + dist1 * dist1);
         },
-        // calculate the current coordinates of a bus between stop1 and stop2
-        calcBusCoords(stop1, stop2, line, dtstr = '1/1/2000') {
-            const curTime = new Date(dtstr + ' ' + this.time);
-            const stop2atime = (stop2.arrival_time === '') ? new Date(dtstr + ' 00:00') : new Date(dtstr + ' ' + stop2.arrival_time);
-            const stop1dtime = (stop1.departure_time === '') ? new Date(dtstr + ' 23:59') : new Date(dtstr + ' ' + stop1.departure_time);
-            const totalTime = stop2atime - stop1dtime;
-            const route = routesList.find((rt) => rt.lineAbbr === line); // shape_length
-            const curTimeMil = curTime - new Date(dtstr + ' :00:00');
-            
-            // const station1 = stopsList.find((station) => station.stopName.replace(/\s/g, '') === stop1.stop_name.replace(/\s/g, ''));
-            // const station2 = stopsList.find((station) => station.stopName.replace(/\s/g, '') === stop2.stop_name.replace(/\s/g, ''));
-            // const segmentLen = this.calcDistBetwPts(station1.coordinates, station2.coordinates);
-            // const scaledLen = (curTimeMil * segmentLen) / totalTime; // use time ratio to calc distance traveled
-            // TODO this should be distace between two station coordinates
+        // calculates point that is dist from pt0 on line between pt0 to pt1
+        interpBetwPts(pt1, pt0, dist) {
+            // calc line and then get distance from first point
+            const slope = (pt1[1] - pt0[1]) / (pt1[0] - pt0[0]);
+            const b = pt0[1] - slope * pt0[0];
+            const x1 = pt0[0] + dist / Math.sqrt(1 + (slope * slope));
+            const y1 = slope * x1 + b;
 
-            const scaledLen = (curTimeMil * route.path_length) / totalTime;
-
-            // sum distance between each stop until we reach the distance traveled, then return those coordinates
-            let sumDist = 0;
-            for (let i = 0; i < route.coordinates.length; i++) {
-                if (i === 0) {
-                    continue;
-                }
-                // calc distance between two points
-                // TODO: why are route coords [[l,lg],[l,lg]] ???
-                const dist0 = route.coordinates[i][0][0] - route.coordinates[i - 1][0][0];
-                const dist1 = route.coordinates[i][0][1] - route.coordinates[i - 1][0][1];
-                // sum of total distance
-                sumDist += Math.sqrt(dist0 * dist0 + dist1 * dist1);
-                if (sumDist >= scaledLen) {
-                    return route.coordinates[i][0];
-                }
+            // calc in both directions on line
+            const x2 = pt0[0] - dist / Math.sqrt(1 + (slope * slope));
+            const y2 = slope * x2 + b;
+            // compare distances from point 1
+            const d1 = this.calcDistBetwPts([x1, y1], pt1);
+            const d2 = this.calcDistBetwPts([x2, y2], pt1);
+            // return the closer one to point 1 (e.g. between the two points)
+            if (d1 <= d2) {
+                return [x1, y1];
             }
-            return '';
+            return [x2, y2];
+        },
+        // calculate the current coordinates of a bus between stop1 and stop2
+        // Note this implementation assumes route segments are in order :/
+        calcBusCoords(stop1, stop2, line, dtstr = '1/1/2000') {
+            // put all times into arbitrary date so we can do math with them
+            const curTime = new Date(dtstr + ' ' + this.time);
+            const stop2arv = (stop2.arrival_time === '') ? new Date(dtstr + ' 00:00') : new Date(dtstr + ' ' + stop2.arrival_time);
+            const stop1dept = (stop1.departure_time === '') ? new Date(dtstr + ' 23:59') : new Date(dtstr + ' ' + stop1.departure_time);
+            const totalTime = stop2arv - stop1dept; // total time spent on route
+            const timeSinceDept = curTime - stop1dept; // minutes since departing stop1
+            const percTime = timeSinceDept / totalTime; // percentage of way bus is through segment
+            
+            const route = routesList.find((rt) => rt.lineAbbr === line); // route obj
+            const scaledDist = percTime * route.path_length; // distance bus has gone on path
+
+            let sumDist = 0;
+            let d1 = 0;
+            let extDist = 0;
+            // sum distance between dept and arv coordinates, saving coords in between
+            for (let i = 0; i < route.coordinates.length; i++) {
+                // distance formula = sqrt((x2-x1)^2 + (y2-y1)^2)
+                d1 = this.calcDistBetwPts(route.coordinates[i][1], route.coordinates[i][0]);
+
+                // use length of summed segments to calculate the distance in map units bus should be from the start coordinate
+                if (sumDist + d1 >= scaledDist) { // see if point should be on this line segment
+                    extDist = scaledDist - sumDist; // distance to go on segment
+                    return this.interpBetwPts(route.coordinates[i][1], route.coordinates[i][0], extDist); // route.coordinates[i][0];
+                }
+                sumDist += d1; // keep running total
+            }
+            return route.coordinates[0][0]; // if fails return stop1 coordinates
         },
     }
 
