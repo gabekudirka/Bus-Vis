0f500ac9146820a9e118fd731661013bef5f94c1
Gabrielius Kudirka <gabekudirka@gmail.com>
2021-12-17 14:35:50 -0700

    changes so that it runs on this repo

--- bus-vis-app/process_data.bat
+++ bus-vis-app/process_data.bat
@@ -1,3 +1,17 @@
+echo Creating directories and procesisng data...
+
+@echo off
+
+mkdir "src/data"
+
+mkdir "src/data/buses"
+
+mkdir "src/data/plans"
+
+mkdir "src/data/stationLocations"
+
+mkdir "src/data/supplementary_data"
+
 mapshaper "data_input/1. Network Data/1. BusRoutes_UTA/BusRoutes_UTA.dbf" "data_input/1. Network Data/1. BusRoutes_UTA/BusRoutes_UTA.prj" "data_input/1. Network Data/1. BusRoutes_UTA/BusRoutes_UTA.shp" "data_input/1. Network Data/1. BusRoutes_UTA/BusRoutes_UTA.shx" -proj wgs84 -o src/data/BusRoutes_UTA.json
 
 mapshaper "data_input/1. Network Data/2. BusStops_UTA/BusStops_UTA.dbf" "data_input/1. Network Data/2. BusStops_UTA/BusStops_UTA.prj" "data_input/1. Network Data/2. BusStops_UTA/BusStops_UTA.shp" "data_input/1. Network Data/2. BusStops_UTA/BusStops_UTA.shx" -proj wgs84 -o src/data/BusStops_UTA.json
@@ -11,3 +25,5 @@ python data_processing/convert_pollutant_data.py
 python data_processing/extract_bus_data.py
 
 python data_processing/extract_plan_data.py
+
+echo Finished
---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+
+mkdir "src/data"
+mkdir "src/data/buses"
+mkdir "../src/data/plans"
+mkdir "../src/data/stationLocations"
+mkdir "../src/data/supplementary_data"
